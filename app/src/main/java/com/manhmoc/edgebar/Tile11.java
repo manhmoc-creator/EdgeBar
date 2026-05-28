dc7cfe0232534917ebdc14e451f66e6c9d2cabaf
manhmoc-ui <24020773@vnu.edu.vn>
2026-05-28 18:30:13 +0000

    V19.12.3.5: THE FINAL MASTERPIECE - Tổng hợp hoàn chỉnh 16 phiên bản, ổn định tuyệt đối

--- app/src/main/java/com/manhmoc/edgebar/Tile11.java
+++ app/src/main/java/com/manhmoc/edgebar/Tile11.java
@@ -2,11 +2,5 @@ package com.manhmoc.edgebar;
 import android.service.quicksettings.TileService;
 import android.content.Intent;
 public class Tile11 extends TileService {
-    @Override public void onClick() {
-        super.onClick();
-        Intent ipc = new Intent("com.manhmoc.edgebar.IPC_ACTION");
-        ipc.putExtra("act", getSharedPreferences("EdgeBarPrefs", MODE_PRIVATE).getString("tile_11_act", "NONE"));
-        sendBroadcast(ipc);
-        getQsTile().setState(android.service.quicksettings.Tile.STATE_INACTIVE); getQsTile().updateTile();
-    }
+    @Override public void onClick() { super.onClick(); Intent ipc = new Intent("com.manhmoc.edgebar.IPC_ACTION"); ipc.putExtra("act", getSharedPreferences("EdgeBarPrefs", MODE_PRIVATE).getString("tile_11_act", "NONE")); sendBroadcast(ipc); getQsTile().setState(android.service.quicksettings.Tile.STATE_INACTIVE); getQsTile().updateTile(); }
 }
