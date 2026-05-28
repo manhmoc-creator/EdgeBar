64855a81cb68b693ca1d0b00656ebcf76bd51f8c
manhmoc-ui <24020773@vnu.edu.vn>
2026-05-28 19:53:30 +0000

    V19.12.1.8 - THE ULTIMATE ARCHITECT: All features merged, optimized, bugs fixed. Multi-Action, 15 QS Tiles, 5 Macros, Independent Moon Core, Live Preview, AppOps ADB info.

--- app/src/main/java/com/manhmoc/edgebar/Tile5.java
+++ app/src/main/java/com/manhmoc/edgebar/Tile5.java
@@ -1,12 +1,25 @@
+
 package com.manhmoc.edgebar;
+
 import android.service.quicksettings.TileService;
+
 import android.content.Intent;
+
 public class Tile5 extends TileService {
+
     @Override public void onClick() {
+
         super.onClick();
+
         Intent ipc = new Intent("com.manhmoc.edgebar.IPC_ACTION");
+
         ipc.putExtra("act", getSharedPreferences("EdgeBarPrefs", MODE_PRIVATE).getString("tile_5_act", "NONE"));
+
         sendBroadcast(ipc);
+
         getQsTile().setState(android.service.quicksettings.Tile.STATE_INACTIVE); getQsTile().updateTile();
+
     }
+
 }
+
