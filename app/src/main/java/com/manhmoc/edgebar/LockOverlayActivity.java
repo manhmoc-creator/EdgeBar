da1d0f36e39a51c2befde0b24ed7b060f7ec8d43
Ban-end <sinendego@gmail.com>
2026-08-04 15:03:46 +0000

    V19.12.3.6.37 - Alan Walker, K-391 & Emelie Hollow - Lily

--- app/src/main/java/com/manhmoc/edgebar/LockOverlayActivity.java
+++ app/src/main/java/com/manhmoc/edgebar/LockOverlayActivity.java
@@ -1,13 +1,13 @@
 
 package com.manhmoc.edgebar;
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Toast;
 import androidx.biometric.BiometricPrompt;
 import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentActivity;
 
-public class LockOverlayActivity extends Activity {
+public class LockOverlayActivity extends FragmentActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
